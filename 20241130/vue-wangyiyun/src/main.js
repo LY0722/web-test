2fd833d29c572fe42718b24bbfc8628d5ef118e7
gulinqi <2640586414@qq.com>
2024-12-08 16:16:14 +0800

    12月8号-ArkTs讲述了get和post请求

--- 20241130/vue-wangyiyun/src/main.js
+++ 20241130/vue-wangyiyun/src/main.js
@@ -1,4 +1,8 @@
 import { createApp } from 'vue'
 import App from './App.vue'
-
+import Vue from 'vue';
+import ElementUI from 'element - ui';
+import 'element - ui/lib/theme - default/index.css';
+import 'element - ui/packages/theme - chalking/src/index.scss';
+Vue.use(ElementUI);
 createApp(App).mount('#app')
